ed414de653f517a3de42272132d81b7514636dc3
hadi <hadi.lok8004@gmail.com>
2019-01-21 17:05:53 +0330

    Initial Commit

---
+++ SwipeCardView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeCardView
+//
+//  Created by Hadi Husseini on 1/21/19.
+//  Copyright © 2019 hadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
