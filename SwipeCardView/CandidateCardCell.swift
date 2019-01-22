290de2481e11641a42a34d4971939f3f49ebbce0
hadi <hadi.lok8004@gmail.com>
2019-01-22 15:12:47 +0330

    remove some files

--- SwipeCardView/CandidateCardCell.swift
+++ SwipeCardView/CandidateCardCell.swift
@@ -12,16 +12,6 @@ import UIKit
 class CandidateCardCell: UITableViewCell {
     @IBOutlet weak var titleView: UIView!
     @IBOutlet weak var title: UILabel!
-    @IBOutlet weak var experienceLbl: UILabel!
-    @IBOutlet weak var locationLbl: UILabel!
-    @IBOutlet weak var salaryLbl: UILabel!
-    @IBOutlet weak var workTypeLbl: UILabel!
-    @IBOutlet weak var educationLbl: UILabel!
-    @IBOutlet weak var workBackgroundLbl: UILabel!
-    @IBOutlet weak var workBackgroundView: UIView!
-    @IBOutlet weak var firstSeparatorView: UIView!
-  //  @IBOutlet weak var tagView: TagView!
-    @IBOutlet weak var skillsHeightConst: NSLayoutConstraint!
     @IBOutlet weak var descLbl: UILabel!
     @IBOutlet weak var descHeight: NSLayoutConstraint!
     
